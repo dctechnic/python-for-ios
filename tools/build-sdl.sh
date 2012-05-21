68e6775e3dba73f93a71e492c1abc96ea45332ff
yep <yep171@yahoo.com>
2012-05-21 22:49:04 +0200

    removed files which do not have to do with building python.

--- tools/build-sdl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. $(dirname $0)/environment.sh
-pushd $KIVYIOSROOT/src/SDL/Xcode-iPhoneOS/SDL
-xcodebuild -project SDLiPhoneOS.xcodeproj -target libSDL -configuration Release -sdk iphoneos$SDKVER
-popd
-
-cp src/SDL/Xcode-iPhoneOS/SDL/build/Release-iPhoneOS/libSDL.a $BUILDROOT/lib
-cp -a src/SDL/include $BUILDROOT/include/SDL 
